32d794226c1122795bcc655b124d63e49638c9f0
Nguyễn Đại Quốc <127086245+NguyenDaiQuoc@users.noreply.github.com>
2025-05-17 11:51:00 +0700

    Update botkiemtragiahanghoa.py

--- botkiemtragiahanghoa.py
+++ botkiemtragiahanghoa.py
@@ -1,9 +1,6 @@
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext
-from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
